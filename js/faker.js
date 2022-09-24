3f756c862f9420f761224b95fb7d87351d4411bd
Chris Pucknell <chris.pucknell@gmail.com>
2022-09-24 17:12:59 +0100

    add checkbox selection for fields, add frameset styling

--- js/faker.js
+++ js/faker.js
@@ -1,80 +1,174 @@
 const { faker } = require("@faker-js/faker");
 let output = document.querySelector(".output");
 let num = document.querySelector("#num");
-let btn = document.querySelector("#gen");
+let settingsContainer = document.querySelector("#settings");
+let options = settingsContainer.querySelectorAll("input[type=checkbox]"); // nodelist of checkboxes
+let generateButton = document.querySelector("#gen");
+let clearButton = document.querySelector("#clear");
+let checkedCount = document.querySelector("#checkedCount");
+let checkAll = document.querySelector("#checkAllOptions");
+let uncheckAll = document.querySelector("#uncheckAllOptions");
+let activeSettings = [];
 
-btn.addEventListener("click", () => {
-  let int = parseInt(num.value);
-  for (let i = 0; i < int; i++) {
-    generate();
-  }
+// Button Event Listeners
+checkAll.addEventListener("click", checkAllOptions);
+uncheckAll.addEventListener("click", uncheckAllOptions);
+clearButton.addEventListener("click", clearResults);
+for (const option of options) {
+    option.addEventListener("click", updateDisplay);
+}
+generateButton.addEventListener("click", () => {
+    clearResults(); // clears onscreen results for each generate to avoid issues
+    let int = parseInt(num.value);
+    for (let i = 0; i < int; i++) {
+        generate(i);
+    }
 });
 
-class Record {
-  constructor(firstName, lastName, email, userName, password) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.userName = userName;
-    this.password = password;
-  }
-
-  static newTable() {
-    let tbl = document.createElement("table");
-    let tblr1 = document.createElement("tr");
-    let record = new Record();
-    for (let key in record) {
-      let tblh = document.createElement("th");
-      let text;
-      if (key.includes("Name")) {
-          text = key.split("Name")
-          text[1] = "Name"
-          console.log("text",text)
-          text[0] = text[0].slice(0,1).toUpperCase() + text[0].slice(1).toLowerCase()
-          text = text.join(" ")
-      } else {
-          text = key.slice(0,1).toUpperCase() + key.slice(1).toLowerCase()
-      }
-      tblh.textContent = text;
-      console.log(tblh);
-      tblr1.appendChild(tblh);
+function checkAllOptions() {
+    for (const option of options) {
+        option.checked = true;
     }
-    tbl.appendChild(tblr1);
-    output.appendChild(tbl);
-  }
-
-  newRow() {
-    console.log("this",this)
-    let tbl = document.querySelector("table");
-    let row = document.createElement("tr");
-    for (const prop in this) {
-        let cell = document.createElement("td");
-        cell.textContent = this[prop];
-        row.appendChild(cell)
+    updateDisplay();
+}
+function uncheckAllOptions() {
+    for (const option of options) {
+        option.checked = false;
     }
-    tbl.appendChild(row);
-  }
+    updateDisplay();
 }
 
-function generate() {
-  try {
-    let firstName = faker.name.firstName();
-    let lastName = faker.name.lastName();
-    let jobTitle = faker.name.jobTitle();
-    let prefix = faker.name.prefix();
-    let suffix = faker.name.suffix();
-    let fullName = `${prefix} ${firstName} ${lastName} ${suffix}`;
-    let jobArea = faker.name.jobArea();
-    let phone = faker.phone.phoneNumber();
-    let email = faker.internet.email(firstName, lastName);
-    let userName = faker.internet.userName(firstName, lastName);
-    let password = faker.internet.password(10, true);
-    // not available in current faker ver
-    // let age = faker.date.birthdate()
-    if (!output.textContent) Record.newTable();
-    let record = new Record(firstName, lastName, email, userName, password);
-    record.newRow()
-  } catch (err) {
+function updateDisplay() {
+    count = 0;
+    activeSettings = [];
+    for (const option of options) {
+        const fieldName = option.value;
+        if (!option.checked) continue; // skip unchecked setting
+        count++;
+        activeSettings.push(fieldName);
+    }
+    checkedCount.textContent = count;
+    console.log({ activeSettings });
+}
+
+function clearResults() {
+    let tbl = document.querySelector("table");
+    if (tbl) {
+        tbl.remove();
+    }
+}
+
+updateDisplay();
+class Record {
+    constructor(
+        index,
+        firstName,
+        lastName,
+        jobTitle,
+        prefix,
+        suffix,
+        fullName,
+        jobArea,
+        phone,
+        email,
+        userName,
+        password
+    ) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.prefix = prefix;
+        this.suffix = suffix;
+        this.fullName = `${prefix} ${firstName} ${lastName} ${suffix}`;
+        this.jobArea = jobArea;
+        this.phone = phone;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    static newTable() {
+        let tbl = document.createElement("table");
+        let tblr = document.createElement("tr");
+
+        for (let field in activeSettings) {
+            const fieldName = activeSettings[field];
+            let tblh = document.createElement("th");
+            let text;
+            if (fieldName.includes("Name")) {
+                text = fieldName.split("Name");
+                text[0] =
+                    text[0].slice(0, 1).toUpperCase() +
+                    text[0].slice(1).toLowerCase();
+                text[1] = "Name";
+            } else if (fieldName.includes("job")) {
+                text = fieldName.split("job");
+                text[0] = "Job";
+                text[1] =
+                    text[1].slice(0, 1).toUpperCase() +
+                    text[1].slice(1).toLowerCase();
+            } else {
+                text =
+                    fieldName.slice(0, 1).toUpperCase() +
+                    fieldName.slice(1).toLowerCase();
+            }
+            Array.isArray(text) && (text = text.join(" "));
+            tblh.textContent = text; // add text
+            // console.log(tblh);
+            tblr.appendChild(tblh);
+        }
+        tbl.appendChild(tblr);
+        output.appendChild(tbl);
+    }
+
+    newRow() {
+        let tbl = document.querySelector("table");
+        let row = document.createElement("tr");
+        for (const prop in this) {
+            if (!activeSettings.includes(prop)) {
+                continue;
+            }
+            let cell = document.createElement("td");
+            cell.textContent = this[prop];
+            row.appendChild(cell);
+        }
+        tbl.appendChild(row);
+    }
+}
+
+function generate(index) {
+    try {
+        let firstName = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        let jobTitle = faker.name.jobTitle();
+        let prefix = faker.name.prefix();
+        let suffix = faker.name.suffix();
+        let fullName = `${prefix} ${firstName} ${lastName} ${suffix}`;
+        let jobArea = faker.name.jobArea();
+        let phone = faker.phone.phoneNumber();
+        let email = faker.internet.email(firstName, lastName);
+        let userName = faker.internet.userName(firstName, lastName);
+        let password = faker.internet.password(10, true);
+        // not available in current faker ver
+        // let age = faker.date.birthdate()
+        if (!output.textContent) Record.newTable(); // if not content in output, create a new table
+        let record = new Record(
+            index,
+            firstName,
+            lastName,
+            jobTitle,
+            prefix,
+            suffix,
+            fullName,
+            jobArea,
+            phone,
+            email,
+            userName,
+            password
+        );
+        record.newRow();
+    } catch (err) {
         throw new Error(err);
-  }
+    }
 }
